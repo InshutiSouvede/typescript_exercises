d0b386362ba0f3c87c39daf1e578f96baf3b5fba
InshutiSouvede <isouvede2@gmail.com>
2024-07-08 16:11:06 +0200

    Example of union in utils.ts

--- couchSurfingWebsite/utils.ts
+++ couchSurfingWebsite/utils.ts
@@ -13,4 +13,20 @@ export function populateUser(isReturning : boolean, userName: string ) {
     }
     userNameDisplay.innerHTML = userName
 }
-export function showProperties(){}
+function add(firstValue:(string|number), secondValue:(string|number)) {
+    let result
+    if (typeof firstValue === 'number' && typeof secondValue === 'number') {
+        result = firstValue + secondValue
+    }
+    if (typeof firstValue === 'string' && typeof secondValue === 'string') {
+        result = firstValue + ' ' + secondValue
+    }
+    if (typeof firstValue === 'number' && typeof secondValue === 'string') {
+        console.log('cannot perform this addition')
+    }
+    if (typeof firstValue === 'string' && typeof secondValue === 'number') {
+        console.log('cannot perform this addition')
+    }
+}
+const combinedReviews = add(5,1)
+const firstNameLastName = add('Souvede', 'Inshuti')
